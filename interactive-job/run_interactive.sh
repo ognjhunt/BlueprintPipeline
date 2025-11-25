6f8c75b6db29bd087bf5d3c91b396fee5e0a9c04
ognjhunt <ohstnhunt@gmail.com>
2025-11-25 15:18:04 +0000

    changed interactive pipeline

--- interactive-job/run_interactive.sh
+++ interactive-job/run_interactive.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-python run_interactive_assets.py
+
+echo "[PHYSX] Starting interactive asset generation"
+python /app/run_interactive_assets.py
+echo "[PHYSX] Done interactive asset generation"
